d8e7198b738ca52fd8c74c5750bbada2c324b814
Albert Gadjaev <albertg@my-clay.com>
2022-04-01 21:34:11 +0200

    Install Mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import Head from "next/head";
-import { AppProps } from "next/app";
+import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
-import "styles/global.css";
+import 'styles/global.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <title>Scroll</title>
+        <meta
+          name='viewport'
+          content='minimum-scale=1, initial-scale=1, width=device-width'
+        />
       </Head>
-      <Component {...pageProps} />
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </>
   );
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
-import { getCssText } from "../stitches.config";
+import { Html, Head, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
 
 const Document = () => {
   return (
     <Html>
       <Head>
         <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>"
-        />
-        <style
-          id="stitches"
-          dangerouslySetInnerHTML={{ __html: getCssText() }}
+          rel='icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📜</text></svg>'
         />
       </Head>
       <body>
@@ -22,5 +17,7 @@ const Document = () => {
     </Html>
   );
 };
+
+Document.getInitialProps = createGetInitialProps();
 
 export default Document;
